468afcd8397f849206ee41d0779f9651a2a44855
飯野亮 <karuros.come@gmail.com>
2019-08-07 19:48:51 +0900

    Revert "Add a Static Pages controller"

--- vendor/bundle/ruby/2.3.0/bin/pumactl.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"C:\Ruby\bin\ruby.exe" "C:/Users/work/sns_app/vendor/bundle/ruby/2.3.0/bin/pumactl" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"C:\Ruby\bin\ruby.exe" "%~dpn0" %*
